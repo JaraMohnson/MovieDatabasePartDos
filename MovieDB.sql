68f3d9d574a532ee9b7fa6fbe127a8453b48ee31
JaraMohnson <jaramohnson@gmail.com>
2022-07-19 17:51:09 -0400

    added MovieDB??

---
+++ MovieDB.sql
@@ -0,0 +1,27 @@
+--CREATE TABLE Movies 
+--(Id INT IDENTITY (1,1) PRIMARY KEY,
+--Title NVARCHAR(50), 
+--Genre NVARCHAR(20),
+--Runtime FLOAT
+--);
+
+--INSERT INTO Movies(Title, Genre, Runtime) 
+--VALUES 
+--('The Conjuring', 'Horror', 120),
+--('The Nun', 'Horror', 100),
+--('The Hunger Games', 'Action', 120), 
+--('Top Gun: Maverick', 'Action', 160),
+--('Encanto', 'Adventure', 105),
+--('Up', 'Adventure', 115),
+--('National Treasure', 'Action', 120),
+--('Catching Fire', 'Action', 145),
+--('Interstellar', 'SciFi', 170), 
+--('Avengers: Endgame', 'Action', 160),
+--('Inside Out', 'Adventure', 110),
+--('Martian', 'SciFi', 150), 
+--('Minions: Rise of Gru', 'Comedy', 90),
+--('Despicable Me', 'Comedy', 95), 
+--('Zootopia', 'Adventure', 100);
+
+
+--SELECT * FROM Movies;
